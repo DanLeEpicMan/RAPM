9684dc2e63899a72f75ab13c9bd242af135b04c9
Dan Le Man <holydoge1337@gmail.com>
2024-05-17 15:32:02 -0700

    Included turnovers in stints (which have PM score of 0). Also moved all datasets into 'data' folder.

--- rapm.py
+++ rapm.py
@@ -6,21 +6,32 @@ import nba_on_court as noc
 from nba_api.stats.endpoints import playbyplayv2
 
 # load data of a specific game
-pbp = playbyplayv2.PlayByPlayV2(game_id="0022100001").play_by_play.get_data_frame()
-pbp_with_players = noc.players_on_court(pbp)
+nbastats = pd.read_csv('data/nbastats_po_2022.csv')
+pbpstats = pd.read_csv('data/pbpstats_po_2022.csv')
+
+nbastats = nbastats.loc[nbastats['GAME_ID'] == 42200405].reset_index(drop=True) 
+pbpstats = pbpstats.loc[pbpstats['GAMEID'] == 42200405].reset_index(drop=True) 
+
+full_data = noc.players_on_court(noc.left_join_nbastats(nbastats, pbpstats))
 
 # extract features we care about
-subset = pbp_with_players[
+subset = full_data[
     [
-        'SCORE',
-        'SCOREMARGIN', 
+        'SCOREMARGIN',
+        'TURNOVERS', 
         'AWAY_PLAYER1', 'AWAY_PLAYER2', 'AWAY_PLAYER3', 'AWAY_PLAYER4', 'AWAY_PLAYER5', 
         'HOME_PLAYER1', 'HOME_PLAYER2', 'HOME_PLAYER3', 'HOME_PLAYER4', 'HOME_PLAYER5'
      ]
 ]
-subset = subset[~subset['SCOREMARGIN'].isna()].replace('TIE', 0).reset_index()
-subset['PM'] = subset['SCOREMARGIN'].astype(np.int64).diff().replace(np.nan, 0)
-subset.at[0, 'PM'] = np.int64(subset.at[0, 'SCOREMARGIN']) # manually add in the first PM
+scores = subset[~subset['SCOREMARGIN'].isna()].replace('TIE', 0).reset_index(drop=True)
+scores['PM'] = scores['SCOREMARGIN'].astype(np.int64).diff().replace(np.nan, 0)
+scores.at[0, 'PM'] = np.int64(scores.at[0, 'SCOREMARGIN']) # manually add in the first PM
+
+turnovers = subset[subset['TURNOVERS'] == 1]
+turnovers['SCOREMARGIN'].fillna(0, inplace=True)
+turnovers['PM'] = np.zeros(len(turnovers), dtype=np.int64)
+
+subset = pd.concat([scores, turnovers], ignore_index=True)
 
 # get indicators of players
 # -1 := away team
