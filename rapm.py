c5126c0b3da8bf4fa074aa364c69837fa866fc3e
Aarya-Kulkarni <aarya_kulkarni@ucsb.edu>
2024-05-24 15:51:21 -0700

    Added design.csv

--- rapm.py
+++ rapm.py
@@ -5,6 +5,7 @@ from sklearn.linear_model import Ridge
 import nba_on_court as noc
 from nba_api.stats.endpoints import playbyplayv2
 from preprocess_tools import preprocess_game, get_design_matrix
+from sklearn.linear_model import ElasticNetCV
 
 
 # load data
@@ -21,12 +22,25 @@ data = pd.concat(list_of_game_data, ignore_index=True)
 
 design_matrix, players = get_design_matrix(data, return_players=True)
 
-model = Ridge(alpha=1).fit(X=design_matrix, y=data['PM'])
+design = pd.DataFrame(
+    data = design_matrix,
+    columns = players
+)
+
+print(design)
+
+#model = Ridge(alpha=1).fit(X=design_matrix, y=data['PM'])
+regr = ElasticNetCV(l1_ratio = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9], cv = 5, random_state = 0)
+regr.fit(design_matrix, data['PM'])
+print(regr.l1_ratio_)
+print(regr.alpha_)
+print(regr.coef_)
 
 results = pd.DataFrame({
         'Player': noc.players_name(players),
-        'RAPM': model.coef_
+        'RAPM': regr.coef_
 })
 
 print(results)
-results.to_csv('results.csv')
+results.to_csv('results.csv') 
+design.to_csv('design.csv') 
